077272599d76d7df5019bbae77ea13735125c9ba
Richard Moore <github@ricmoo.com>
2019-06-27 02:46:41 -0400

    Use multiple files for better cross-domain caching..

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Create the flattened CSS
+npx meeseeks flatten style.css > ./dist/style.css
+
+# Copy minified scripts
+cp ./node_modules/ethers/dist/ethers.min.js ./dist/ethers.js
+cp ./lib/dist/takoyaki.min.js ./dist/takoyaki.js
+npx uglifyjs ./script.js --output ./dist/script.js
+
+# Copy HTML and modify it to point external files to https://takoyaki.cafe/ (better caching)
+cp ./index.html ./dist/index.html
+sed -E -e 's/data:prod="-" ([a-z]+)="([^"]+)\//\1="https:\/\/takoyaki.cafe\//g' -i .tmp ./dist/index.html
+rm ./dist/index.html.tmp
