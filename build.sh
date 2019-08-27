c9927bfac168e8ed07b7989f8c8e41f23f9dee8c
Richard Moore <github@ricmoo.com>
2019-08-27 18:47:02 -0400

    Moved static dist build to heroku-app static folder.

--- build.sh
+++ build.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 # Create the flattened CSS
-python -c "import base64; spinner = base64.b64encode(file('./spinner.gif').read()); print file('./style.css').read().replace('./spinner.gif', 'data:image/gif;base64,' + spinner)" > ./dist/style.css
+python -c "import base64; spinner = base64.b64encode(file('./spinner.gif').read()); print file('./style.css').read().replace('./spinner.gif', 'data:image/gif;base64,' + spinner)" > ./heroku-app/static/style.css
 
 # Copy minified scripts
-cp ./node_modules/ethers/dist/ethers.min.js ./dist/ethers.js
-cp ./lib/dist/takoyaki.min.js ./dist/takoyaki.js
-npx uglifyjs ./script.js --output ./dist/script.js
+cp ./node_modules/ethers/dist/ethers.min.js ./heroku-app/static/ethers.js
+cp ./lib/dist/takoyaki.min.js ./heroku-app/static/takoyaki.js
+npx uglifyjs ./script.js --output ./heroku-app/static/script.js
 
 # Copy HTML and modify it to point external files to https://takoyaki.cafe/ (better caching)
-cp ./index.html ./dist/index.html
-sed -E -e 's/data:prod="-" ([a-z]+)="([^"]+)\//\1="https:\/\/takoyaki.cafe\//g' -i .tmp ./dist/index.html
-rm ./dist/index.html.tmp
+cp ./index.html ./heroku-app/static/index.html
+sed -E -e 's/data:prod="-" ([a-z]+)="([^"]+)\//\1="https:\/\/takoyaki.cafe\//g' -i .tmp ./heroku-app/static/index.html
+rm ./heroku-app/static/index.html.tmp
 
-cp ./history.js ./dist/history.js
+cp ./history.js ./heroku-app/static/history.js
+
